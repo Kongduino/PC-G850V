6d89c7f9546f07db90b0df477bc33aa31ad04ebe
kongduino <dbvic@me.com>
2021-05-05 01:53:45 +0800

    Slightly modified (stereo) version of test_01.c

--- test_01.c
+++ test_01.c
@@ -1,4 +1,8 @@
 110 /* test_01.c */
+111 /* https://cutt.ly/hbbB2UL */
+112 /* BY @hangyodon1123 */
+103 /* updated by @Kongduino */
+
 120 main() {
 130 int k;
 140 void c_dotp(int,int);
@@ -12,20 +16,19 @@
 220 printf("[%c]\n",k);
 230 printf("%XH\n",k);
 240 c_dotp(20,6);
-
+250 }
 260 }
-270 }
 
-280 void c_dotp(int a,int b) {
-290 int i,j;
-300 for (i = 0; i < 6; i++) {
-310 for (j = 0; j < 8; j++) {
-320 line(a+i*b,j*b,a+i*b+b-2,j*b+b-2,0,65535,1+point(i+6,j));
-330 pset(i+6,j,1);
-340 a=a+40;
-350 line(a+i*b,j*b,a+i*b+b-2,j*b+b-2,0,65535,1+point(i+6,j));
-360 pset(i+6,j,1);
-365 a=a-40;
+270 void c_dotp(int a,int b) {
+280 int i,j;
+290 for (i = 0; i < 6; i++) {
+300 for (j = 0; j < 8; j++) {
+310 line(a+i*b,j*b,a+i*b+b-2,j*b+b-2,0,65535,1+point(i+6,j));
+320 pset(i+6,j,1);
+330 a=a+40;
+340 line(a+i*b,j*b,a+i*b+b-2,j*b+b-2,0,65535,1+point(i+6,j));
+350 pset(i+6,j,1);
+360 a=a-40;
 370 }
 380 }
 390 }
